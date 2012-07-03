4ed970f257f9ab1886c720826575db2986317ad9
Raghuveera Betina <rbetina@gmail.com>
2012-07-03 08:17:58 -0500

    Rolled back lists.rb.

--- lists.rb
+++ lists.rb
@@ -3,14 +3,7 @@ student2 = {"name" => "Grant",    "hometown" => "Chicago"}
 student3 = {"name" => "Michael",  "hometown" => "Houston"}
 student4 = {"name" => "Joseph",   "hometown" => "Seattle"}
 
-# students_array = [student1, student2, student3, student4]
-
-students_array = Array.new
-students_array << student1
-students_array << student2
-students_array << student3
-students_array << student4
-
+students_array = [student1, student2, student3, student4]
 
 # This method accepts a hash and formats it for output.
 def display(student)
